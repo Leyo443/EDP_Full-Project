e356cdb86fe0cc66a671904a8125b2c400ab3f5d
Leyo443 <vlgmidel@pcu.edu.ph>
2026-03-17 11:45:59 +0800

    added credentials and home form

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,6 +20,11 @@ namespace MIDEL_VinceLeonardoG._BSIT_2D
             InitializeComponent();
         }
 
+        string[,] userCredentials =
+        {
+            {"admin","admin","justinevince" },
+            {"cashier","password","roksi" }
+        };
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -33,29 +39,59 @@ namespace MIDEL_VinceLeonardoG._BSIT_2D
         {
             if (textBox1.Text == "")
             {
-                MessageBox.Show("Please enter username!","Validation");
+                MessageBox.Show("Please enter username!", "Validation");
                 textBox1.Focus();
             }
             else if (textBox2.Text == " ")
             {
-                MessageBox.Show("Please enter Password!","Validation");
+                MessageBox.Show("Please enter Password!", "Validation");
                 textBox2.Focus();
             }
-               
-        }
-                private void textBox2_TextChanged(object sender, EventArgs e)
+            else
+                MessageBox.Show("Welcome" + "! " + textBox1.Text);
+            {
+                for (int x = 0; x < userCredentials.GetLength(0); x++)
                 {
+                    if (textBox1.Text == userCredentials[x, 0])
+                    {
+                        if (textBox2.Text == userCredentials[x, 1])
+                        {
+                            frmHome frm = new frmHome();
+                            MessageBox.Show("Welcome " + userCredentials[x, 2]);
+                            this.Hide();
+                            frm.Show();
+                            break;
+                        }
 
+                        else
+                        {
+                            MessageBox.Show("Invalid Username/Password");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+            private void textBox2_TextChanged(object sender, EventArgs e)
+                        {
+
+                        }
+
+                        private void label2_Click_1(object sender, EventArgs e)
+                        {
+
+                        }
+
+                        private void label1_Click(object sender, EventArgs e)
+                        {
+
+                        }
+                    }
                 }
 
-        private void label2_Click_1(object sender, EventArgs e)
-        {
+ 
+        
+                
+            
 
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
-}
